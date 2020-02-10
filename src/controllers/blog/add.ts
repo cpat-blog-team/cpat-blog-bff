2c0898c3e583056babe8f5065704574611e903e7
isaiah-santala <isaiah.santala@ibm.com>
2020-02-10 12:14:10 -0800

    updated fields for controller and models to include summary and username, fixed bug with eslint, added script to package.json jest watch

--- src/controllers/blog/add.ts
+++ src/controllers/blog/add.ts
@@ -3,9 +3,9 @@ import handleErrorMiddleware from '../../middleware/handle-error-middleware';
 import Blog from '../../models/Blog';
 
 const add: RequestHandler = async (req, res) => {
-	const { userId, name, title, content, version, createdAt, updatedAt } = req.body;
+	const { userId, name, title, content, version, createdAt, updatedAt, username, summary } = req.body;
 
-	const blog = new Blog({ userId, name, title, content, version, createdAt, updatedAt });
+	const blog = new Blog({ userId, username, name, title, content, summary, version, createdAt, updatedAt });
 	await blog.save();
 
 	res.send({
--- src/models/Blog.ts
+++ src/models/Blog.ts
@@ -2,21 +2,25 @@ import { Document, Model, Schema, model } from 'mongoose';
 
 export interface IBlog extends Document {
 	userId: string;
+	username: string;
 	name: string;
 	title: string;
 	content: string;
+	summary: string;
 	version: number;
 	createdAt: Date;
 	updatedAt: Date;
 }
 
-interface IBlogModel extends Model<IBlog> {}
+interface IBlogModel extends Model<IBlog> { }
 
 const schema = new Schema({
 	userId: { type: String, required: true },
+	username: { type: String, required: true },
 	name: { type: String, required: true },
 	title: { type: String, required: true },
 	content: { type: String, required: true },
+	summary: { type: String, required: true },
 	version: { type: Number, required: true },
 	createdAt: { type: Date, required: false },
 	updatedAt: { type: Date, required: false }
