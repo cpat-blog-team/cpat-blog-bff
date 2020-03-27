4f7f41ff9e1f9e9d3adb6a811975b41449b82334
isaiah-santala <isaiah.santala@ibm.com>
2020-03-27 13:44:06 -0700

    updated seed tto set content as stringified quill delta. fixed order of blogs respons to be newest to oldest

--- tests/utils/examplePost.ts
+++ tests/utils/examplePost.ts
@@ -1,3 +1,5 @@
+import Delta from 'quill-delta'
+  ;
 interface Post {
   name: string;
   email: string;
@@ -12,15 +14,24 @@ export const examplePost: Post = {
   name: 'bob',
   title: 'bobs blog',
   summary: 'bobs summary',
-  content: 'bobs content',
+  content: JSON.stringify(new Delta([{ insert: 'bobs content' }])),
   version: 1
 };
+
+const numberedPost = (number: number) => ({
+  email: 'bob@bob.com',
+  name: 'bob',
+  title: `bobs blog ${number}`,
+  summary: 'bobs summary',
+  content: JSON.stringify(new Delta([{ insert: 'bobs content' }])),
+  version: 1
+});
 
 export const exampleList = (numberOfPosts: number) => {
   const list: Post[] = [];
 
   for (let c = 0; c < numberOfPosts; c++) {
-    list.push(examplePost);
+    list.push(numberedPost(c));
   }
 
   return list;
