3d60f3dfb98ad66e17c9024a4d33b291d0a65d32
isaiah-santala <isaiah.santala@ibm.com>
2020-04-16 10:25:43 -0700

    updates wipeDB route to also wipe all documents in the community guidelines collection

--- src/controllers/seed/wipeDB.ts
+++ src/controllers/seed/wipeDB.ts
@@ -1,10 +1,12 @@
 import handleErrorMiddleware from '../../middleware/handle-error-middleware';
 import Blog from '../../models/Blog';
+import CommunityGuidelines from '../../models/CommunityGuidelines';
 import { RequestHandler } from 'express';
 
 const wipeDB: RequestHandler = async (req, res) => {
   try {
     await Blog.deleteMany({});
+    await CommunityGuidelines.deleteMany({});
     res.sendStatus(202);
   } catch (err) {
     res.status(500).send(err);
--- tests/controllers/seed/wipeDB.test.ts
+++ tests/controllers/seed/wipeDB.test.ts
@@ -1,12 +1,13 @@
 import * as SeedController from '../../../src/controllers/seed';
 import request from 'request-promise';
-import { exampleList, makeQuery } from '../../utils';
+import { exampleList, makeQuery, exampleGuidelines } from '../../utils';
 
-describe('Seed method at endpoint "/dev/seed"', () => {
+describe('Seed method at endpoint "/dev/wipeDB"', () => {
   beforeAll(async (done) => {
     // seed DB with exampleList
     const list = exampleList(5);
     await request(makeQuery('POST', '/dev/seedManyBlogs', JSON.stringify({ blogs: list })));
+    await request(makeQuery('POST', '/communityGuidelines', JSON.stringify(exampleGuidelines)));
     done();
   });
 
@@ -20,7 +21,10 @@ describe('Seed method at endpoint "/dev/seed"', () => {
 
     // gets all blogs to check that they are empty
     const { blogs } = JSON.parse(await request(makeQuery('GET', '/blogs', null)));
+
+    const { communityGuidelines } = JSON.parse(await request(makeQuery('GET', '/communityGuidelines', null)));
     expect(blogs).toEqual([]);
+    expect(communityGuidelines).toEqual(null);
     done();
   });
 });
