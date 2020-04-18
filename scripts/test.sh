4d9d956fd5b172b398eb0fb7c516dd6ec42ca038
popcor255 <popcor255@gmail.com>
2020-04-17 20:38:28 -0400

    update script and make it less verbose

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+set -e
+
+cleanup() {
+    `(docker-compose -f docker-compose.dev.yml down)` > /dev/null 2>&1
+    trap '' EXIT INT TERM
+    exit $err
+}	
+
+trap cleanup SIGINT EXIT
+
 docker-compose -f docker-compose.dev.yml up -d --force-recreate --remove-orphans
 echo "loading database ..."
 sleep 3
