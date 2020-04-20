72f2458dca3753ec5379f3c8fcc31c8a263b0620
popcor255 <popcor255@gmail.com>
2020-04-20 12:51:37 -0400

    There is a small bug
    The terminal would not line break after ending the script early. I added a clean up function to fix this

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
+
+set -e
+
+#This silences the terminal while the docker is being pulled down
+cleanup() {
+    docker_compose_pid > /dev/null 2>&1
+    trap '' EXIT INT TERM
+    exit $err
+}	
+
 docker-compose -f docker-compose.dev.yml up --force-recreate --remove-orphans
+docker_compose_pid=$!
