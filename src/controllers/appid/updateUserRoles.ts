bb8a7b8c8727c214c142caaa211f378275e8977b
popcor255 <popcor255@gmail.com>
2020-04-29 13:38:44 -0400

    add special field none to remove roles

--- src/controllers/appid/updateUserRoles.ts
+++ src/controllers/appid/updateUserRoles.ts
@@ -7,32 +7,32 @@ import getToken from './getToken';
 require('dotenv').config();
 
 const updateUserRoles: RequestHandler = async (req, res) => {
-  const token = await getToken();
-  const myHeaders = { Authorization: `Bearer ${token}` };
+	const token = await getToken();
+	const myHeaders = { Authorization: `Bearer ${token}` };
 
-  const { id } = req.params;
-  if (!id) return res.status(400).send('Error missing params: id');
+	const { id } = req.params;
+	if (!id) return res.status(400).send('Error missing params: id');
 
-  const { roles } = req.body;
-  if (!roles) return res.status(400).send('Error missing property in body: roles');
+	const { role } = req.body;
+	if (!role) return res.status(400).send('Error missing property in body: role');
 
-  try {
-    // put user's roles by id
-    const response = JSON.parse(
-      await request(
-        makeJsonQuery(
-          'PUT',
-          myHeaders,
-          `https://us-south.appid.cloud.ibm.com/management/v4/${process.env.TENNANT_ID}/users/${id}/roles`,
-          { roles }
-        )
-      )
-    );
+	try {
+		// put user's roles by id
+		const response = JSON.parse(
+			await request(
+				makeJsonQuery(
+					'PUT',
+					myHeaders,
+					`https://us-south.appid.cloud.ibm.com/management/v4/${process.env.TENNANT_ID}/users/${id}/roles`,
+					{ roles: { names: role === 'none' ? [] : [ role ] } }
+				)
+			)
+		);
 
-    res.status(200).send(response);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+		res.status(200).send(response);
+	} catch (err) {
+		res.status(400).send(err);
+	}
 };
 
 export default handleErrorMiddleware(updateUserRoles);
--- tests/controllers/appid/updateUserRoles.test.ts
+++ tests/controllers/appid/updateUserRoles.test.ts
@@ -5,37 +5,32 @@ import { makeQuery } from '../../utils';
 import { exitOnError } from 'winston';
 
 describe('This tests if you can update Users roles by their appid id from the appID controller', () => {
-  test('Test to see if AppID controller has updateUserRoles property', () => {
-    jest.spyOn(AppIDController, 'updateUserRoles');
-  });
+	test('Test to see if AppID controller has updateUserRoles property', () => {
+		jest.spyOn(AppIDController, 'updateUserRoles');
+	});
 
-  test('Should be able to remove all roles from user by passing empty array', async (done) => {
-    const roleNames: string[] = [];
-    const permissions = JSON.stringify({
-      roles: {
-        names: roleNames
-      }
-    });
+	test('Should be able to remove all roles by passing the role "none"', async (done) => {
+		const role: string = 'none';
+		const permissions = JSON.stringify({ role });
 
-    const { roles } = JSON.parse(await request(makeQuery('PUT', `/appid/roles/${process.env.TEST_ID}`, permissions)));
+		const { roles } = JSON.parse(
+			await request(makeQuery('PUT', `/appid/roles/${process.env.TEST_ID}`, permissions))
+		);
 
-    expect(roles).toBe(null);
-    done();
-  });
+		expect(roles).toBe(null);
+		done();
+	});
 
-  test('Should be able to add roles to user by passing array of role names', async (done) => {
-    const roleNames: string[] = ['moderator', 'admin'];
-    const permissions = JSON.stringify({
-      roles: {
-        names: roleNames
-      }
-    });
+	test('Should be able to add roles to user by passing array of role names', async (done) => {
+		const role: string = 'moderator';
+		const permissions = JSON.stringify({ role });
 
-    const { roles } = JSON.parse(await request(makeQuery('PUT', `/appid/roles/${process.env.TEST_ID}`, permissions)));
-    // filter out matching roles from roleNames variable
-    const filteredRoles = roles.filter(({ name }: any) => name !== roleNames.shift());
+		const { roles } = JSON.parse(
+			await request(makeQuery('PUT', `/appid/roles/${process.env.TEST_ID}`, permissions))
+		);
 
-    expect(filteredRoles.length).toBe(0);
-    done();
-  });
+		expect(roles.length).toBe(1);
+		expect(roles.pop().name).toBe(role);
+		done();
+	});
 });
